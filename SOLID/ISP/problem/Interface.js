403851775a98e307bfb36834a67d2c93ed148160
shawn-thecool <shawn-dev@naver.com>
2019-12-18 18:23:17 +0900

    Feat : LSP & ISP 이론 공부 추가 리스코프 치환 원치은 아직 개념적으로 이해가 더 필요해 보인다. 대신 인터페이스 분리의 원칙을 공부해 봄 스크립트로 인터페이스를 구현해 보아야 겠다.

---
+++ SOLID/ISP/problem/Interface.js
@@ -0,0 +1,14 @@
+class Interface {
+  constructor(props) {
+  }
+ 
+  // ISP 를 구현하기 앞서 ES6 Simbol 을 이용해서
+  // interface 를 구현해 보자
+
+  // SRP 가 클래스간의 단일 책임을 강조했다면
+  // ISP 는 인터페이스간의 단일 책임을 강조하는 것.
+  // 우선은 인터페이스를 만들어 보자
+  
+}
+
+module.exports = Interface
